389397eb67ced1dc61dd1930d525abf54dc0e97f
Kasim Sumra <kasimsumra07@gmail.com>
2022-05-13 15:20:12 +0530

    Day 3 1:23:12

--- lib/screens/all_tasks.dart
+++ lib/screens/all_tasks.dart
@@ -1,12 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:task_managment/colors/app_colors.dart';
+import 'package:task_managment/widgets/button_widget.dart';
+import 'package:task_managment/widgets/task_widget.dart';
 
 class AllTasks extends StatelessWidget {
   const AllTasks({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    List myData = ["Try Harder", "Try Smater", "Try Best", "Think Big"];
+    final leftEditIcon = Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      color: const Color(0xFF2e3253).withOpacity(0.5),
+      child: const Icon(
+        Icons.edit,
+        color: Colors.white,
+      ),
+      alignment: Alignment.centerLeft,
+    );
+    final righDeletIcon = Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      color: Colors.red,
+      child: const Icon(
+        Icons.delete,
+        color: Colors.white,
+      ),
+      alignment: Alignment.centerRight,
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
@@ -63,6 +86,71 @@ class AllTasks extends StatelessWidget {
                 )
               ],
             ),
+          ),
+          Flexible(
+            child: ListView.builder(
+                itemCount: myData.length,
+                itemBuilder: ((context, index) {
+                  return Dismissible(
+                    background: leftEditIcon,
+                    secondaryBackground: righDeletIcon,
+                    onDismissed: (DismissDirection direction) {
+                      print("After Dismiss");
+                    },
+                    confirmDismiss: (DismissDirection direction) async {
+                      if (direction == DismissDirection.startToEnd) {
+                        showModalBottomSheet(
+                            backgroundColor: Colors.transparent,
+                            barrierColor: Colors.transparent,
+                            context: context,
+                            builder: (_) {
+                              return Container(
+                                height: 550,
+                                decoration: BoxDecoration(
+                                    color: const Color(0xFF2e3253)
+                                        .withOpacity(0.5),
+                                    borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(20),
+                                        topRight: Radius.circular(20))),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20, right: 20),
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      ButtonWidget(
+                                          backgroundcolor: AppColors.mainColor,
+                                          text: "View",
+                                          textColor: Colors.white),
+                                      const SizedBox(
+                                        height: 20,
+                                      ),
+                                      ButtonWidget(
+                                          backgroundcolor: AppColors.mainColor,
+                                          text: "Edit",
+                                          textColor: Colors.white),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            });
+                        return false;
+                      } else {
+                        return Future.delayed(Duration(seconds: 1),
+                            () => direction == DismissDirection.endToStart);
+                      }
+                    },
+                    key: ObjectKey(index),
+                    child: Container(
+                      margin: const EdgeInsets.only(
+                          left: 20, right: 20, bottom: 10),
+                      child: TaskWidget(
+                        text: myData[index],
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  );
+                })),
           )
         ],
       ),
